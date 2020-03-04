fbc801523168f31108e148c1bba64ecd37c04197
Philipp Krause <phil.fhs11@googlemail.com>
2020-03-04 20:18:05 +0100

    ADD: device uptime as information

--- subtasks/network_tracker.py
+++ subtasks/network_tracker.py
@@ -2,16 +2,16 @@ import time
 from rgbmatrix import graphics
 import queue
 import ping3
-import threading
+import collections
 import settings
 
 
-class DeviceTracker:
+class NetworkTracker:
 
     def __init__(self):
         self.new_devices = queue.Queue()
         self.old_devices = queue.Queue()
-        self.active_devices = list()
+        self.active_devices = {}
 
     def ping(self, ip, n):
         counter = 0
@@ -22,11 +22,9 @@ class DeviceTracker:
                 counter += 1
             if not result:  # inactive
                 counter += 1
-        if counter == 0:
-            return True
-        return False
+        return counter
 
-    def init(self, add_loop):
+    def init(self, add_loop, rmv_loop):
         add_loop(4, self.display_connected)
 
     def service(self, add_event):
@@ -34,16 +32,16 @@ class DeviceTracker:
             for n in range(1, 255):
                 ip = "192.168.0.{0}".format(n)
                 result = self.ping(ip, 1)
-                if not result:  # inactive
+                if result == 1:  # inactive
                     if ip in self.active_devices:
-                        if not self.ping(ip, 3):
-                            self.active_devices.remove(ip)
+                        if self.ping(ip, 3) == 3:
+                            self.active_devices.pop(n)
                             self.old_devices.put(ip)
                             add_event(2, self.display_old)
                 else:  # active
                     if ip not in self.active_devices:
-                        if self.ping(ip, 3):
-                            self.active_devices.append(ip)
+                        if self.ping(ip, 3) == 0:
+                            self.active_devices[n] = time.perf_counter()
                             self.new_devices.put(ip)
                             add_event(2, self.display_new)
 
@@ -58,11 +56,15 @@ class DeviceTracker:
         text_color = graphics.Color(0, 150, 120)
 
         swap.Clear()
+        line = 0
 
         graphics.DrawText(swap, font, 0, font.baseline, text_color, "Network:")
 
-        for i in range(len(self.active_devices)):
-            graphics.DrawText(swap, font, 0, font.baseline + ((i + 1) * 8), ip_color, self.active_devices[i])
+        for ip in collections.OrderedDict(sorted(self.active_devices.items())):
+            online_duration = int((time.perf_counter() - self.active_devices[ip]) / 60)
+            text = str(ip) + " " * (3 - len(str(ip))) + " up " + str(online_duration) + "min"
+            graphics.DrawText(swap, font, 0, font.baseline + ((line + 1) * 8), ip_color, text)
+            line += 1
 
         matrix.SwapOnVSync(swap)
         time.sleep(3)
@@ -71,9 +73,13 @@ class DeviceTracker:
             for r in range((len(self.active_devices) - 3) * 8 + 1):
                 matrix.Clear()
                 graphics.DrawText(swap, font, 0, font.baseline - r, text_color, "Network:")
+                line = 0
 
-                for i in range(len(self.active_devices)):
-                    graphics.DrawText(swap, font, 0, font.baseline + ((i + 1) * 8) - r, ip_color, self.active_devices[i])
+                for ip in collections.OrderedDict(sorted(self.active_devices.items())):
+                    online_duration = int((time.perf_counter() - self.active_devices[ip]) / 60)
+                    text = str(ip) + " " * (3 - len(str(ip))) + " up " + str(online_duration) + "min"
+                    graphics.DrawText(swap, font, 0, font.baseline + ((line + 1) * 8) - r, ip_color, text)
+                    line += 1
 
                 matrix.SwapOnVSync(swap)
                 time.sleep(0.2)
