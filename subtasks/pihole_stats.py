644a6bb5e9c11311f6aa67962fe700e15575e8db
brokelyn <phil.fhs11@googlemail.com>
2020-10-20 16:09:27 +0000

    ADD: pihole statisctics submodule

---
+++ subtasks/pihole_stats.py
@@ -0,0 +1,45 @@
+import time
+from subtask import Subtask
+import settings
+import requests
+from rgbmatrix import graphics
+from PIL import Image, ImageSequence
+
+
+class PiholeStats(Subtask):
+
+    def __init__(self, add_loop, rmv_loop, add_event):
+        super().__init__(add_loop, rmv_loop, add_event)
+
+        add_loop(4, self.display_stats)
+
+    def display_stats(self, matrix):
+        try:
+            image = Image.open(settings.IMAGES_PATH + 'pi-hole.png')
+            image = image.resize((8, 9))
+        except FileNotFoundError:
+            print('Pi-Hole image not found')
+            return
+
+        for i in range(3):
+            response = requests.get("http://" + settings.PI_HOLE_IP + "/admin/api.php?summaryRaw")
+            json = response.json()
+            queries_today = json['dns_queries_today']
+            percentage_blocked = json['ads_percentage_today']
+
+            swap = matrix.CreateFrameCanvas()
+
+            font = graphics.Font()
+            font.LoadFont(settings.FONT_PATH + "6x13B.bdf")
+            font2 = graphics.Font()
+            font2.LoadFont(settings.FONT_PATH + "6x13.bdf")
+
+            graphics.DrawText(swap, font, 5, font.baseline - 2, graphics.Color(120, 120, 120), " Pi Hole")
+            graphics.DrawText(swap, font, 0, font.baseline * 2 - 2, graphics.Color(0, 180, 120), "Query")
+            graphics.DrawText(swap, font2, 35, font.baseline * 2 - 2, graphics.Color(0, 180, 20), str(queries_today))
+            graphics.DrawText(swap, font, 0, font.baseline * 3 - 1, graphics.Color(120, 20, 20), "Blocked")
+            graphics.DrawText(swap, font2, 47, font.baseline * 3 - 1, graphics.Color(120, 0, 0), str(percentage_blocked)[:2] + '%')
+
+            matrix.SwapOnVSync(swap)
+            matrix.SetImage(image.convert('RGB'))
+            time.sleep(2.5)
