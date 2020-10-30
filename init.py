bc52a0e9bf30e915e1d9c5b5cc2584d9f4f84e9d
brokelyn <phil.fhs11@googlemail.com>
2020-10-30 18:17:43 +0100

    UPDATE: init flags to create a smoother display

--- init.py
+++ init.py
@@ -17,13 +17,13 @@ def init_leds():
     options.parallel = 1
     options.row_address_type = 0
     options.multiplexing = 0
-    options.brightness = 70
-    options.pwm_bits = 11
-    options.pwm_lsb_nanoseconds = 170
+    options.brightness = 85
+    options.pwm_bits = 8
+    options.pwm_lsb_nanoseconds = 130
     options.led_rgb_sequence = 'RGB'
     options.pixel_mapper_config = ''
     options.panel_type = ''
-    options.limit_refresh_rate_hz = 60  # reduce flickering due to high energy consumption
+    options.limit_refresh_rate_hz = 59  # reduce flickering due to high energy consumption
     options.show_refresh_rate = 0
     options.gpio_slowdown = 4
     options.disable_hardware_pulsing = False
